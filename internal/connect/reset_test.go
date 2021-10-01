b1373342f477f43d435644a136bbe80e03b28ca2
Jim Tsao <jimtsao@users.noreply.github.com>
2021-10-01 15:15:00 +0200

    Add test for DropAndRecreateDB()

---
+++ internal/connect/reset_test.go
@@ -0,0 +1,46 @@
+package connect_test
+
+import (
+	"context"
+	"fmt"
+	"strconv"
+	"testing"
+	"time"
+
+	"github.com/justasable/pgmock/internal/connect"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDropAndRecreateDB(t *testing.T) {
+	// connect to database
+	conn, err := connect.Connect()
+	assert.NoError(t, err)
+	defer conn.Close(context.Background())
+
+	// create table with data
+	now := int(time.Now().Unix())
+	testTable := "test" + strconv.Itoa(now)
+	_, err = conn.Exec(context.Background(), fmt.Sprintf(`CREATE TABLE public.%s (id integer)`, testTable))
+	assert.NoError(t, err)
+
+	// run wipe data function
+	conn.Close(context.Background())
+	err = connect.DropAndRecreateDB()
+	assert.NoError(t, err)
+
+	// reconnect to database
+	conn, err = connect.Connect()
+	assert.NoError(t, err)
+	defer conn.Close(context.Background())
+
+	// check if data has been wiped
+	var exists bool
+	err = conn.QueryRow(context.Background(), `
+	SELECT EXISTS (
+		SELECT FROM information_schema.tables 
+		WHERE  table_schema = 'public'
+		AND    table_name   = $1
+	)`, testTable).Scan(&exists)
+	assert.NoError(t, err)
+	assert.False(t, exists)
+}
